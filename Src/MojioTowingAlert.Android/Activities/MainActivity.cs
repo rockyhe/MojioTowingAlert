e8b2a1f8a497d07d0dd2e2feb4748e9a65411c41
Rocky He <rocky.jj.he@gmail.com>
2013-11-13 22:56:55 -0800

    Fixed some compilation issues from copying over Tim's files

--- Src/MojioTowingAlert.Android/Activities/MainActivity.cs
+++ Src/MojioTowingAlert.Android/Activities/MainActivity.cs
@@ -6,7 +6,7 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 
-namespace MojioTowingAlertApp
+namespace MojioTowingAlert.Android
 {
 	[Activity (Label = "@string/applicationName", MainLauncher = true)]
 	public class MainActivity : Activity
@@ -18,12 +18,13 @@ namespace MojioTowingAlertApp
 			base.OnCreate (bundle);
 
 			// Set our view from the "main" layout resource
-			SetContentView (Resource.Layout.SpashScreen);
+			SetContentView (Resource.Layout.SplashScreen);
 
 			// Get our button from the layout resource,
 			// and attach an event to it
 			Button button = FindViewById<Button> (Resource.Id.myButton);
-			
+
+			int count = 0;
 			button.Click += delegate {
 				button.Text = string.Format ("{0} clicks!", count++);
 			};
