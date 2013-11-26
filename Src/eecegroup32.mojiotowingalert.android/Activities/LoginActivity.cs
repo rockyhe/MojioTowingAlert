0c8743e91847157134c4d422428389bd15bfc56c
Rocky He <rocky.jj.he@gmail.com>
2013-11-26 14:07:09 -0800

    Added comments to LoginActivity and BaseActivity

--- Src/eecegroup32.mojiotowingalert.android/Activities/LoginActivity.cs
+++ Src/eecegroup32.mojiotowingalert.android/Activities/LoginActivity.cs
@@ -24,7 +24,7 @@ namespace eecegroup32.mojiotowingalert.android
 			base.OnCreate(bundle);
 			SetContentView(Resource.Layout.Login);
 
-			//Get button from layout and attach event handler
+			//Get the login button from Resource and add a click event handler
 			loginButton = FindViewById<Button>(Resource.Id.logInButton);
 			loginButton.Click += new EventHandler(OnLoginClicked);
 
@@ -38,27 +38,29 @@ namespace eecegroup32.mojiotowingalert.android
 
 		private void OnLoginClicked(object sender, EventArgs e)
 		{
+			//Get the username and password entry text fields from Resource
 			EditText username = FindViewById<EditText>(Resource.Id.usernameEntry);
 			EditText password = FindViewById<EditText>(Resource.Id.passwordEntry);
 
 			//Check if username or password is empty
 			if (string.IsNullOrEmpty(username.Text) || string.IsNullOrEmpty(password.Text))
 			{
-				ErrorMessage (Resource.String.missingUsernameOrPassword);
+				ErrorMessage(Resource.String.missingUsernameOrPassword);
 				return;
 			}
 
+			//Deactivate the button before calling async login
 			loginButton.Activated = false;
 
-			//Submit login request
+			//Submit an async login request
 			Client.SetUserAsync(username.Text, password.Text).ContinueWith(r => {
 				MojioResponse<Mojio.Token> response = r.Result;
 
 				RunOnUiThread(() => {
-					//Re-activate login button
+					//Reactivate login button
 					loginButton.Activated = true;
 
-					//Check if login was a success or not
+					//If login succeeds, go to main menu. Otherwise reactivate button and show error message.
 					if (Client.IsLoggedIn())
 					{
 						GotoMainMenu();
